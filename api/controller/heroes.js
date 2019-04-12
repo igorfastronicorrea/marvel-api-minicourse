8914f2556e5d2f1443ce527fd01707e4f35e4d99
Igor Fastroni <igor.fastroni@gmail.com>
2019-04-12 00:02:42 -0300

    fix image

--- api/controller/heroes.js
+++ api/controller/heroes.js
@@ -12,14 +12,32 @@ const formatHeroes = (marvelResults) => {
     return marvelResults.data.map((hero) => {
         return Object.assign({}, {
             name: hero.name,
-            thumbnail: hero.thumbnail.path,
+            thumbnail: hero.thumbnail.path + "/portrait_xlarge.jpg",
         })
     });
 };
 
 router.get('/all', async (req, res) =>{
-    const heroes = formatHeroes(await marvel.characters.findAll());
-    return res.json(heroes);
+    
+    let spider = formatHeroes(await marvel.characters.findByName('Spider-man'));
+    let iron = formatHeroes(await marvel.characters.findByName('iron man'));
+    let hulk = formatHeroes(await marvel.characters.findByName('hulk'));
+    let capitaoAmerica = formatHeroes(await marvel.characters.findByName('Captain America'));
+    let viuvaNegra = formatHeroes(await marvel.characters.findByName('Black Widow'));
+    let thor = formatHeroes(await marvel.characters.findByName('thor'));
+
+    
+    const heroesAll = Object.assign({}, {heroes: {spider, iron, hulk, capitaoAmerica, viuvaNegra, thor}}) 
+    return res.json(heroesAll);
+});
+
+router.get('/meta', async (req, res) =>{
+    const metas = await marvel.metas.findAll();
+    return res.metas
+});
+
+router.get('/img/:path', async (req, res) => {
+    
 });
 
 module.exports = app => app.use('/heroes', router);
